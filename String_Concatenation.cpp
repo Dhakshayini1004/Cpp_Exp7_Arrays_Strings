715992ee4b01e8ca4463195f98418e2851d4c81b
Dhakshayini Usha R <dhakshayiniusha@gmail.com>
2025-08-06 20:49:43 +0530

    Create String_Concatenation.cpp

---
+++ String_Concatenation.cpp
@@ -0,0 +1,27 @@
+/*Name : Dhakshayini Usha R
+ENTC A2
+PRN : 24070123039
+*/
+#include <iostream>
+#include <cstring>
+using namespace std; 
+
+int main() {
+    //String Concatenation
+    string str1,str2;
+    
+    cout<<"Enter a word :"<<"\t";
+    cin>>str1;
+    cout<<"Enter a word: "<<"\t";
+    cin>>str2;
+    
+    cout<<"Concatenation :"<<str1+str2;
+
+    return 0;
+}
+
+/*Sample Output: 
+Enter a word :	Dhakshayini 
+Enter a word: 	Usha
+Concatenation :DhakshayiniUsha
+*/
