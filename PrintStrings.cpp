565d8931e1f2832904cf0e3b55c8880b92f77850
Dhakshayini Usha R <dhakshayiniusha@gmail.com>
2025-08-06 20:44:23 +0530

    Create PrintStrings.cpp

---
+++ PrintStrings.cpp
@@ -0,0 +1,25 @@
+/*Name : Dhakshayini Usha R
+ENTC A2
+PRN : 24070123039
+*/
+#include <iostream>
+#include <cstring>
+using namespace std; 
+
+int main() {
+    char str1[15] = "Dhakshayini";
+    char str2[7] = {'h','e','l','l','o','\0'};
+    char str3[] = {'h','e','l','l','o','\0'};
+    string str4 = "Dhakshayini";
+    
+    cout<<str1<<endl<<str2<<endl<<str3<<endl<<str4;
+
+    return 0;
+}
+
+/*Sample Output: 
+Dhakshayini
+hello
+hello
+Dhakshayini
+*/
